ec235c9a1f8025d67d3240ed6819ef1be5859957
Eterna <one.eternity@gmail.com>
2017-12-29 23:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object[key]= value
+  return Object.assign({}, object)
 }
